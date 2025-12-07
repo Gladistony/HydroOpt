2641545daa8d6c07f3b5700640362e113a1693a7
Gladistony Silva Lins <gladistony.silva@ufrpe.br>
2025-12-07 12:55:58 -0300

    Redes e Diametros

--- HydroOpt/__init__.py
+++ HydroOpt/__init__.py
@@ -1,1 +1,6 @@
-from .core import TestePublicacao
+from .core import testar_ldiametro, testar_rede, executar_todos_testes
+from .rede import Rede
+from .diametros import LDiametro
+
+__version__ = "0.1.0"
+__all__ = ['Rede', 'LDiametro', 'testar_ldiametro', 'testar_rede', 'executar_todos_testes']
