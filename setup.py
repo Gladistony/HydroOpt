3278e7f51047cb2e1708eebcdad9da285ffda1e9
Gladistony Silva Lins <gladistony.silva@ufrpe.br>
2025-12-07 12:25:06 -0300

    Ajustes de dependencias e descrição

--- setup.py
+++ setup.py
@@ -5,12 +5,18 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hydroopt",  
-    version="0.0.1",         
+    version="0.1.0",         
     author="Gladistony Silva Lins",
-    description="Pacote de teste de publicação",
+    description="Biblioteca de otimização de redes hidráulicas (EPANET) utilizando algoritmos de Inteligência de Enxame (GWO, WOA, PSO) para minimização de custos e garantia de pressão.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
     python_requires='>=3.6',
-    install_requires=[], 
+    install_requires=[
+        "numpy>=1.20.0",
+        "pandas>=1.3.0",
+        "wntr>=1.0.0",
+        "mealpy>=2.5.0",
+        "openpyxl",
+    ], 
 )
